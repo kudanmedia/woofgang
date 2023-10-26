d6195f26b43ae1f38e959f818af67f92f9befb9d
Kudakwashe Nyengera <mckuda@gmail.com>
2023-10-26 11:06:47 +0200

    fixed the navigations buttons

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import Logo from "./woofgang-logo.png"
+import Logo from './woofgang-logo.png';
 import DogImage from './DogImage';
 import { fetchImages } from './Api';
 
@@ -9,71 +9,66 @@ const Gallery = () => {
   const [page, setPage] = useState(1);
   const [searchedBreed, setSearchedBreed] = useState('');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const fetchedImages = await fetchImages(breed, page);
-      setImages(fetchedImages);
-    };
-
-    fetchData();
-  }, [breed, page]);
+  const handleSearch = async () => {
+    setPage(1);
+    setSearchedBreed(breed);
+    const fetchedImages = await fetchImages(breed, 1);
+    setImages(fetchedImages);
+  };
 
   const handleNextPage = () => {
     if (page < 5) {
       setPage(page + 1);
+      handleSearch();
     }
   };
-
+  
   const handlePreviousPage = () => {
     if (page > 1) {
       setPage(page - 1);
+      handleSearch();
     }
   };
+  
 
-  const handleSearch = () => {
-    setPage(1);
-    fetchImages(breed, 1);
-  };
+  useEffect(() => {
+    handleSearch();
+  }, []);
 
   return (
     <div>
       <div className="mb-4 bg-red-400 p-4 mx-auto flex justify-between items-center">
         <div>
-        <DogImage src={Logo} alt="Woof Gang" />
+          <DogImage src={Logo} alt="Woof Gang" />
         </div>
         <div>
-        <input
-          type="text"
-          placeholder="Enter breed (e.g., hound)"
-          value={breed}
-          onChange={(e) => setBreed(e.target.value)}
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') {
-              handleSearch();
-            }
-          }}
-          className="w-96 p-2 border rounded"
-        />
-        <button onClick={handleSearch} className="bg-black text-white px-4 py-2 rounded ml-2">
-          Search
-        </button>
+          <input
+            type="text"
+            placeholder="Enter breed (e.g., hound)"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            className="w-96 p-2 border rounded"
+          />
+          <button onClick={handleSearch} className="bg-black text-white px-4 py-2 rounded ml-2">
+            Search
+          </button>
         </div>
         <div className="justify-items-end">
-        <button
-          onClick={handlePreviousPage}
-          className="bg-black text-white px-4 py-2 rounded mr-2"
-          disabled={page === 1}
-        >
-          Previous
-        </button>
-        <button
-          onClick={handleNextPage}
-          className="bg-black text-white px-4 py-2 rounded"
-          disabled={page === 5}
-        >
-          Next
-        </button>
-      </div> 
+          <button
+            onClick={handlePreviousPage}
+            className="bg-black text-white px-4 py-2 rounded mr-2"
+            disabled={page === 1}
+          >
+            Previous
+          </button>
+          <button
+            onClick={handleNextPage}
+            className="bg-black text-white px-4 py-2 rounded"
+            disabled={page === 5}
+          >
+            Next
+          </button>
+        </div>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
